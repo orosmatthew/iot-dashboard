fdf2ae3450a86b89d3af8e3d3ab81915f588750a
orosmatthew <orosmatthew@pm.me>
2023-02-21 10:17:53 -0500

    Round temp

--- scripts/temp.py
+++ scripts/temp.py
@@ -9,7 +9,7 @@ try:
     temp_c = dht_device.temperature
     temp_f = temp_c * (9 / 5) + 32
     humidity = dht_device.humidity
-    print(str(temp_f) + " " + str(humidity))
+    print(f'{temp_f:.1f} {humidity:.1f}')
 except RuntimeError as error:
     print("error")
 
